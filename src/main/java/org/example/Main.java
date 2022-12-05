36bb398bbebc450264c70cf8edff26db6e3b400b
CyclonApps <119296514+CyclonApps@users.noreply.github.com>
2022-12-05 13:27:55 +0100

    Final Ejemplo 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        CalculatorService calculadora = (CalculatorService) context.getBean("calculatorService");
+
+        // EJEMPLO 1
+        // Opción 1. Crear un objeto de forma normal
+        //Calculadora service = new Calculadora();
+
+        // Opción 2. Recibir un objeto de Spring
+        Calculadora calculadora = (Calculadora) context.getBean("calculadora");
 
         String texto = calculadora.holaMundo();
         System.out.println(texto);
+
+        Calculadora calculadora2 = (Calculadora) context.getBean("calculadora");
+        texto = calculadora2.holaMundo();
+        System.out.println(texto);
+
+        // EJEMPLO 2
+
     }
 }
