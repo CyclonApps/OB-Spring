a39a8049db5e65e02cc44a403858040649e5f939
CyclonApps <119296514+CyclonApps@users.noreply.github.com>
2022-12-07 14:49:20 +0100

    Tercer proyecto

---
+++ ob-springdatajpa/src/main/java/com/example/obspringdatajpa/Coche.java
@@ -0,0 +1,15 @@
+package com.example.obspringdatajpa;
+
+public class Coche {
+    // atributos
+    private Long id;
+    private String manufacturer;
+    private String model;
+    private Integer year;
+
+    // constructores
+
+    // getter y setter
+
+    // tostring
+}
