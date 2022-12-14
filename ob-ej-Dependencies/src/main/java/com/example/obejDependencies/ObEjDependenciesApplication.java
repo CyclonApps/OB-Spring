73d85c9f476e0ec43b313db08b8b73a6cbfb840c
Paunetum <pau.garcia.ext@gmali.com>
2022-12-14 09:26:43 +0100

    Dependencies with application properties

---
+++ ob-ej-Dependencies/src/main/java/com/example/obejDependencies/ObEjDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.obejDependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObEjDependenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObEjDependenciesApplication.class, args);
+	}
+
+}
---
+++ ob-ej-Dependencies/src/test/java/com/example/obejDependencies/ObEjDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obejDependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObEjDependenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
