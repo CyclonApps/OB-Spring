c785c59fbdaceadf30f9997d3650f97a26b5aeaa
CyclonApps <119296514+CyclonApps@users.noreply.github.com>
2022-12-19 14:37:12 +0100

    Response Entity

--- ob-rest-datajap/src/main/java/com/example/obrestdatajap/controller/BookController.java
+++ ob-rest-datajap/src/main/java/com/example/obrestdatajap/controller/BookController.java
@@ -2,10 +2,13 @@ package com.example.obrestdatajap.controller;
 
 import com.example.obrestdatajap.entities.Book;
 import com.example.obrestdatajap.repository.BookRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -34,6 +37,20 @@ public class BookController {
     }
 
     // Buscar un solo libro en base de datos segun su id
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable Long id) {
+        Optional<Book> bookOpt = bookRepository.findById(id);
+
+        // Opción 1
+        if (bookOpt.isPresent()) {
+            return ResponseEntity.ok(bookOpt.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Opción 2
+        // return bookOpt.orElse(null);
+    }
 
     // Crear un nuevo libro en base a datos
 
