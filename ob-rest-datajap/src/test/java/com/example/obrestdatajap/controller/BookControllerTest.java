37b5b6826b5e2437f465138a282ceb72b9a411ad
Paunetum <pau.garcia.ext@gmali.com>
2022-12-29 11:00:15 +0100

    book controller create

--- ob-rest-datajap/src/test/java/com/example/obrestdatajap/controller/BookControllerTest.java
+++ ob-rest-datajap/src/test/java/com/example/obrestdatajap/controller/BookControllerTest.java
@@ -1,14 +1,18 @@
 package com.example.obrestdatajap.controller;
 
+import com.example.obrestdatajap.entities.Book;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +33,7 @@ class BookControllerTest {
         testRestTemplate = new TestRestTemplate(restTemplateBuilder);
     }
 
+    @DisplayName("Comprobar hola mundo desde controladores Spring REST")
     @Test
     void hello() {
         ResponseEntity<String> response =
@@ -40,13 +45,48 @@ class BookControllerTest {
 
     @Test
     void findAll() {
+        ResponseEntity<Book[]> response =
+            testRestTemplate.getForEntity("/api/books", Book[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(200, response.getStatusCodeValue());
+
+        List<Book> books = Arrays.asList(response.getBody());
+        System.out.println(books.size());
     }
 
     @Test
     void findOneById() {
+        ResponseEntity<Book> response =
+                testRestTemplate.getForEntity("/api/books/9", Book.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
     void create() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        String json = """
+                {
+                    "title": "Libro creado desde Spring Test",
+                    "author": "Yuval Noah",
+                    "pages": 650,
+                    "price": 19.99,
+                    "releaseDate": "2019-12-01",
+                    "online": false
+                }
+                """;
+
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+
+        ResponseEntity<Book> response = testRestTemplate.exchange("/api/books", HttpMethod.POST, request, Book.class);
+
+        Book result = response.getBody();
+
+        assertEquals(1L, result.getId());
+        assertEquals("Libro creado desde Spring Test", result.getTitle());
     }
 }
