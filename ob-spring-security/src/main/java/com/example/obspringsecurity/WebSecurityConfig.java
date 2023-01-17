53ee09523df614d25d996c216a0f4a6ac4e6a252
Paunetum <pau.garcia.ext@gmali.com>
2023-01-17 12:52:56 +0100

    WebSecurityConfig v2

--- ob-spring-security/src/main/java/com/example/obspringsecurity/WebSecurityConfig.java
+++ ob-spring-security/src/main/java/com/example/obspringsecurity/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package com.example.obspringsecurity;
 
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -24,18 +30,7 @@ import java.util.Iterator;
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/hola").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin()
-                .and().httpBasic();
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsManager users(DataSource dataSource) {
+    public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails user1 = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("password")
@@ -47,83 +42,19 @@ public class WebSecurityConfig {
                 .roles("USER", "ADMIN")
                 .build();
 
-        Collection<UserDetails> users = new Collection<UserDetails>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<UserDetails> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(UserDetails userDetails) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends UserDetails> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public boolean equals(Object o) {
-                return false;
-            }
-
-            @Override
-            public int hashCode() {
-                return 0;
-            }
-        };
-
-        return new InMemoryUserDetailsManager(users);
+        return new InMemoryUserDetailsManager(user1, user2);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/hola").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+
+        return http.build();
+    }
+
+
 }
